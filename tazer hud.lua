f4fefa649c5c4dd54b1134f88dd00facec75c5fe
pashamaladec <pashamail79@gmail.com>
2021-11-13 16:12:13 +0300

    19.12.2018

--- tazer hud.lua
+++ tazer hud.lua
@@ -2,27 +2,44 @@ font_flag = require('moonloader').font_flag
 font = renderCreateFont('Giger', 20, font_flag.BOLD)
 local sampev = require 'lib.samp.events'
 
+function tinfo()
+end
+
 function main()
 	 if not isSampfuncsLoaded() or not isSampLoaded() then return end
 	 while not isSampAvailable() do wait(100) end
-	 sampAddChatMessage("{21ff00}Tazer{ff704d}Hud{ffffff} | loaded", -1)
+	 sampAddChatMessage("{21ff00}Tazer{ff704d}Hud{ffffff} | Загружен, автоматически активируется при начале рабочего дня. Включить/выключить - {8c00ff}/thud", -1)
+	 sampRegisterChatCommand("thud", day)
 	 ton = 0
 	 toff = 1
+	 rab = 0
 	 while true do
 			 wait(0)
 			 if not isPauseMenuActive() and isPlayerPlaying(playerHandle) then
+				 if rab == 1 then
 				 if ton == 1 then
-					 	renderFontDrawText(font, 'Tazer {19ff00}on', 42, 850, 0xffffffff, false)
-					end
+					 	renderFontDrawText(font, 'Tazer {19ff00}on', 42, 1035, 0xffffffff, false)
+				end
+			end
+		end
+		if rab == 1 then
 					if toff == 1 then
-						renderFontDrawText(font, 'Tazer {ff704d}off', 42, 850, 0xffffffff, false)
+						renderFontDrawText(font, 'Tazer {ff704d}off', 42, 1035, 0xffffffff, false)
 					end
+				end
 					end
-			 end
-			 end
+				end
 
-function sampev.onSendCommand(msg)
-if msg == "/tazer" then
+function sampev.onServerMessage(tclr, txt)
+	if txt:find('Рабочий день начат') then
+		rab = 1
+		elseif txt:find('Рабочий день окончен') then
+		rab = 0
+	end
+end
+
+function sampev.onSendCommand(cmd)
+if cmd == "/tazer" then
 	if ton == 0 and toff == 1 then
 			ton = 1
 			toff = 0
@@ -31,4 +48,16 @@ if msg == "/tazer" then
 				toff = 1
 			end
 		end
+			end
+
+function day()
+	if rab == 0 then
+		rab = 1
+		days = "включён"
+		else rab = 0
+			days = "выключен"
+	end
+	ton = 0
+	toff = 1
+sampAddChatMessage('{21ff00}Tazer{ff704d}Hud{ffffff} | Состояние скрипта сменено. Сейчас он {8c00ff}'..days..'', -1)
 end
