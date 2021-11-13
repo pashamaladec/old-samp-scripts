7300cfdce46a8c04f03fc82f4cfbf5593a7e820c
pashamaladec <pashamail79@gmail.com>
2021-11-13 16:22:31 +0300

    26.12.2018, v4.1

--- tazer hud.lua
+++ tazer hud.lua
@@ -12,15 +12,17 @@ local sampev = require 'lib.samp.events'
 	 soff = '{ff0202}OFF' 									-- состояние худа ВЫКЛ def.	'{ff0202}OFF'
 	 son = '{19ff00}ON' 									-- состояние худа ВКЛ def. '{19ff00}ON'
 	 st = 'Tazer'											-- надпись перед ВКЛ/ВЫКЛ
-	 ton = 0 												-- переменная для включения осн. худа def. 0
-	 toff = 1 												-- переменная для выключения осн. худа def. 1
-	 rab = 0 												-- переменная отображения рабочего дня def. 0
-	 timeron = 1											-- переменная вкл/выкл таймера def. 1
+	 timeron = 1											-- вкл/выкл таймера def. 1
 	 cx = 42												-- координата X осн. худа def. 42
 	 cy = 528 												-- координата Y осн. худа def. 528
 	 tcx = 58 												-- координата X таймера def. 58
 	 tcy = 512 												-- координата Y таймера def. 512
-	 state = soff 											-- !НЕ ТРОГАТЬ! изначальное состояние худа
+	 -- !НЕ ТРОГАТЬ! -- 
+	 state = soff 											-- изначальное состояние худа
+	 opt = 0
+	 ton = 0 												-- переменная для включения осн. худа def. 0
+	 toff = 1 												-- переменная для выключения осн. худа def. 1
+	 rab = 0 												-- переменная отображения рабочего дня def. 0
 	 -- НАСТРОЙКИ СКРИПТА
 
 function main()
@@ -29,6 +31,7 @@ function main()
 	 sampAddChatMessage("{21ff00}Tazer{ff704d}Hud{ffffff} | Загружен, автоматически активируется при начале рабочего дня. Помощь - {8c00ff}/thelp", -1)
 	 sampRegisterChatCommand("thud", day)
 	 sampRegisterChatCommand("thelp", guide)
+	 sampRegisterChatCommand("tcoord", opt)
 
 	 while true do
 			 wait(0)
@@ -39,11 +42,31 @@ function main()
 					 	renderFontDrawText(tazer, ''..st..' '..state..'', cx, cy, 0xffffffff, false)
 					end
 				end
+				 	if opt == 1 then
+				 	cux, cuy = getCursorPos()
+				 	cux = cux+15
+				 	cuy = cuy+5
+					renderFontDrawText(tazer, ''..st..' '..state..'', cux, cuy, 0xffffffff, false)
+					renderFontDrawText(timer, '{ff4300}X: '..cux..' {4286f4}Y: '..cuy, cux, cuy+20, 0xffffffff, false)
+					if isKeyJustPressed(VK_LBUTTON) then
+						sampAddChatMessage(thud..'{ff4300}X: '..cux..' {4286f4}Y: '..cuy..'', -1)
+					end
+				end
 			end
 		end
 
+function opt()
+	if opt == 0 then
+		opt = 1
+		showCursor(true, true)
+		else
+			showCursor(false,false) 
+			opt = 0
+		end
+end
+
 function guide()
-	sampShowDialog(0, '{ffd505}TazerHud', '{ffffff}/thud{a0a0a0} - вкл/выкл основного худа\n{ffffff}/tguide{a0a0a0} - гайд по скрипту, этот диалог\n \n'..tg..'Для того что бы настроить положение худа, таймера или прочего..\n   нужно открыть скрипт блокнотом и изменить подписанные параметны\n'..tg..'Время для таймера - время на компьютере \n \n'..tgg..'Если состояние тазера {8c00ff}[ON/OFF]{ffffff} сбилось..\n  нужно выключить скрипт {8c00ff}[/thud]{ffffff}, переключить тазер {8c00ff}[/tazer]{ffffff} и снова включить\n', 'CLOSE', '', 4)
+	sampShowDialog(0, '{ffd505}TazerHud', '{ffffff}/thud{a0a0a0} - вкл/выкл основного худа\n{ffffff}/thelp{a0a0a0} - гайд по скрипту, этот диалог\n{ffffff}/tcoord {a0a0a0} - какая никакая настройка. Выводит на экран координаты \n'..tg..'Для того что бы настроить положение худа, таймера или прочего..\n   нужно открыть скрипт блокнотом и изменить подписанные параметны\n'..tg..'Время для таймера - время на компьютере \n \n'..tgg..'Если состояние тазера {8c00ff}[ON/OFF]{ffffff} сбилось..\n  нужно выключить скрипт {8c00ff}[/thud]{ffffff}, переключить тазер {8c00ff}[/tazer]{ffffff} и снова включить\n', 'CLOSE', '', 4)
 end
 
 function sampev.onServerMessage(tclr, txt)
@@ -84,4 +107,4 @@ function day()
 	ton = 0
 	toff = 1
 sampAddChatMessage(thud..'Состояние скрипта сменено. Сейчас он {8c00ff}'..days, -1)
-endравоылфарвыфолар
+end
